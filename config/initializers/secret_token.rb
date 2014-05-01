89178f53c022c424d2371c000d95e9527691e6ba
Victor Plascencia <vplascencia@chicagotechacademy.org>
2014-05-01 14:36:43 -0500

    initial commit
    first commit of the app. awwwww :) they grow up so fast

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fortyweekexam::Application.config.secret_token = '9ae62e12b4ccc208a1095f10ee2e281999c1bb0af82ba0c5d5ea9107ecff7e21adcf87deaa9b7bf5ea9b717d4abf14802ae781e94ee601f439790db87f4a4ace'
